1f97bf64ceed0de09a13f03d698ace5825f5f3bd
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-09-24 10:03:50 -0400

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (element) => {
+  const estudante = {
+    name: element,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudante;
+};
 module.exports = createStudent;
